8619609f3be5a474ef9753f98e2cc09a6129c3e7
OSengirbay <osengirbay@almanit.kz>
2020-10-18 18:25:36 +0600

    Banner background color fixin

--- resources/views/site/home/banner_main.blade.php
+++ resources/views/site/home/banner_main.blade.php
@@ -6,7 +6,7 @@
         <div class="owl-carousel owl-theme">
             @foreach($banners as $banner)
                 <div class="item">
-                    <a href="{{$banner->redirect}}" title="{{$banner->title}}" class="d-block" style="@if($banner->background_color)background-color {{$banner->background_color}};@endif background-image: url({{url($banner->file_url)}});">
+                    <a href="{{$banner->redirect}}" title="{{$banner->title}}" class="d-block" style="@if($banner->background_color)background-color: {{$banner->background_color}};@endif background-image: url({{url($banner->file_url)}});">
                     </a>
                 </div>
             @endforeach
