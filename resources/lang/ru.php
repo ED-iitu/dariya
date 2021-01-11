ffc39af0043b42ee8937c730fe0c23420d3217f2
OSengirbay <osengirbay@almanit.kz>
2021-01-11 22:53:50 +0600

    fix password reset template

--- resources/lang/ru.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'welcome' => 'Добро пожаловать на мой сайт!',
-    'contacts' => 'Контакты',
-    'fovarites' => 'Избранные',
-    'You are receiving this email because we received a password reset request for your account.' => 'Вы получили это письмо, потому что мы получили запрос на сброс пароля для вашей учетной записи.'
-];
